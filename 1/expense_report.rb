c20d44ec177095670922f201dcbab3543a6304b2
Alex Pech <alexpech12@gmail.com>
2020-12-02 09:26:55 +1100

    Day 1 performance improvment - change permutation to combination

--- 1/expense_report.rb
+++ 1/expense_report.rb
@@ -3,7 +3,7 @@ require_relative '../read_file'
 def expense_report(number = 2)
   input = read_file('input.txt', &:to_i)
 
-  result = input.permutation(number).find { |group| group.sum == 2020 }
+  result = input.combination(number).find { |group| group.sum == 2020 }
 
   puts "#{result} adds up to 2020"
   puts "Product of result is #{result.reduce(:*)}"
