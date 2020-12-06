76ac08d17b0e70072cd74fc7b5df520fbf69f153
Alex Pech <alexpech12@gmail.com>
2020-12-06 16:51:28 +1100

    Add a new shared file reading utility for reading groups in files

--- read_file.rb
+++ read_file.rb
@@ -9,3 +9,30 @@ def read_file(filename)
   end
   output
 end
+
+def read_file_groups(filename)
+  output = []
+  group = ''
+  File.foreach(filename).with_index do |line, i|
+    if line.chomp.empty? && i != 0
+      output << if block_given?
+                  yield(group)
+                else
+                  group
+                end
+      group = ''
+    else
+      group += line.chomp
+    end
+  end
+
+  unless group.empty?
+    output << if block_given?
+      yield(group)
+    else
+      group
+    end
+  end
+
+  output
+end
