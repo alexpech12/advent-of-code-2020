1a322b9975d6be60ad626557b2516b8d4d0a146d
Alex Pech <alexpech12@gmail.com>
2020-12-08 16:23:43 +1100

    Move BagRule class to separate file

--- 7/part_1.rb
+++ 7/part_1.rb
@@ -1,46 +1,6 @@
 require_relative '../read_file.rb'
+require_relative 'bag_rules.rb'
 require 'set'
-
-class BagRule
-  class ContainingBagRule
-    attr_reader :amount, :colour
-
-    class << self
-      def parse(str)
-        ContainingBagRule.new(
-          str.split(' ').first.to_i,
-          str.split(' ')[1..2].join(' ')
-        )
-      end
-    end
-
-    def initialize(amount, colour)
-      @amount = amount
-      @colour = colour
-    end
-  end
-
-  class << self
-    def parse(str)
-      bag_colour, containing_bag_strings = str.split(' bags contain ')
-      containing_bags =
-        containing_bag_strings.split(', ').map { |colour| ContainingBagRule.parse(colour) }
-
-      BagRule.new(bag_colour, containing_bags)
-    end
-  end
-
-  attr_reader :bag_colour, :containing_bags
-
-  def initialize(bag_colour, containing_bags)
-    @bag_colour = bag_colour
-    @containing_bags = containing_bags
-  end
-
-  def containing_colours
-    containing_bags.map(&:colour)
-  end
-end
 
 # Map all the bag rules to their containing colours for easy rule lookup
 containing_colour_map = {}
